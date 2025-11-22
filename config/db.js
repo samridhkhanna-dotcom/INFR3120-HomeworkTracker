b1461876eaa41e74ae8182a30854db652568f05c
samridhkhanna-dotcom <samridh.khanna@ontariotechu.net>
2025-11-21 23:56:46 -0500

    db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
-// db connection file
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    console.log("Connecting using:", process.env.DB_URI);
+    await mongoose.connect(process.env.DB_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Error:", err);
+  }
+};
+
+module.exports = connectDB;
