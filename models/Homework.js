b7d3472c904f7bcda79d6cbfa11342b476ec1e9a
samridhkhanna-dotcom <samridh.khanna@ontariotechu.net>
2025-11-22 00:05:45 -0500

    Homework.js

--- models/Homework.js
+++ models/Homework.js
@@ -1,1 +1,29 @@
-// model
+// models/Homework.js
+
+const mongoose = require("mongoose");
+
+const HomeworkSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
+    default: "Not Started",
+  },
+  description: {
+    type: String,
+    required: false,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Homework", HomeworkSchema);
